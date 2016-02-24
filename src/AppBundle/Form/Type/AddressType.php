72751f4d4db964dcaf2777fdb8491ccbdbbd86c9
Layton E <layton.everson@gmail.com>
2016-02-24 16:33:15 -0700

    built address form type

--- src/AppBundle/Form/Type/AddressType.php
+++ src/AppBundle/Form/Type/AddressType.php
@@ -1,15 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: layton
- * Date: 2/23/16
- * Time: 3:10 PM
- */
 
 namespace AppBundle\Form;
 
+use AppBundle\Form\Type\StateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Model\AddressModel;
 
-class AddressForm
+class AddressForm extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('street_line_one', TextType::class)
+            ->add('city', TextType::class)
+            ->add('state', StateType::class)
+            ->add('postal_code', TextType::class)
+        ;
 
+        if ($options['show_address_two']) {
+            $builder->add('street_line_two', TextType::class);
+
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => AddressModel::class,
+            'show_address_two' => false,
+        ]);
+    }
 }
