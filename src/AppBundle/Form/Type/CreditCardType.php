5ae4ba42727a9523d04b03873222ff512eba2d40
Layton E <layton.everson@gmail.com>
2016-02-24 15:53:30 -0700

    Added credit card form type

--- src/AppBundle/Form/Type/CreditCardType.php
+++ src/AppBundle/Form/Type/CreditCardType.php
@@ -1,15 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: layton
- * Date: 2/23/16
- * Time: 3:05 PM
- */
 
 namespace AppBundle\Form;
 
+use AppBundle\Form\Type\CardExpirationMonthType;
+use AppBundle\Form\Type\CardExpirationYear;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Model\CreditCardModel;
 
-class CreditCardForm
+class CreditCardForm extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('card_number', TextType::class)
+            ->add('expiration_month', CardExpirationMonthType::class)
+            ->add('expiration_year', CardExpirationYear::class)
+            ->add('security_code', TextType::class)
+        ;
 
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CreditCardModel::class
+        ]);
+    }
 }
