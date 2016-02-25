f33ff6fcc257d32f38dcff31f254226556f676c0
Layton E <layton.everson@gmail.com>
2016-02-24 17:16:17 -0700

    Added prospect form and updated option on address form

---
+++ src/AppBundle/Form/ProspectType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Model\PersonModel;
+
+class ProspectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => $options['show_labels'] ? 'First Name' : false,
+                'attr' => ['placeholder' => $options['show_placeholders'] ? 'First Name' : null]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => $options['show_labels'] ? 'Last Name' : false,
+                'attr' => ['placeholder' => $options['show_placeholders'] ? 'Last Name' : null]
+            ])
+            ->add('shippingAddress', AddressType::class, [
+                'show_labels' => $options['show_labels'],
+                'show_placeholders' => $options['show_placeholders']
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => $options['show_labels'] ? 'Phone Number' : false,
+                'attr' => [
+                    'placeholder' => $options['show_placeholders'] ? '(###) ###-####' : null,
+                    'class' => 'phone'
+                ]
+            ])
+            ->add('emailAddress', EmailType::class, [
+                'label' => $options['show_labels'] ? 'Email Address' : false,
+                'attr' => [
+                    'placeholder' => $options['show_placeholders'] ? 'email@domain.com' : null,
+                    'class' => 'emailAddress',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => PersonModel::class,
+            'show_labels' => true,
+            'show_placeholders' => true,
+        ]);
+    }
+}
--- src/AppBundle/Form/Type/AddressType.php
+++ src/AppBundle/Form/Type/AddressType.php
@@ -9,19 +9,35 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use AppBundle\Model\AddressModel;
 
-class AddressForm extends AbstractType
+class AddressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('street_line_one', TextType::class)
-            ->add('city', TextType::class)
-            ->add('state', StateType::class)
-            ->add('postal_code', TextType::class)
+            ->add('streetLineOne', TextType::class, [
+                'label' => $options['show_labels'] ? 'Address' : false,
+                'attr' => ['placeholder' => $options['show_placeholders'] ? 'Address' : null]
+            ])
+            ->add('city', TextType::class, [
+                'label' => $options['show_labels'] ? 'City' : false,
+                'attr' => ['placeholder' => $options['show_placeholders'] ? 'City' : null]
+            ])
+            ->add('state', StateType::class, [
+                'label' => $options['show_labels'] ? 'State' : false,
+                'attr' => ['placeholder' => $options['show_placeholders'] ? 'State' : null]
+            ])
+            ->add('postalCode', TextType::class, [
+                'label' => $options['show_labels'] ? 'Zip Code' : false,
+                'attr' => ['placeholder' => $options['show_placeholders'] ? 'Zip Code' : null]
+            ])
         ;
 
         if ($options['show_address_two']) {
-            $builder->add('street_line_two', TextType::class);
+            $builder->add('streetLineTwo', TextType::class, [
+                'label' => $options['show_labels'] ? 'Apt / Unit' : false,
+                'attr' => ['placeholder' => $options['show_placeholders'] ? '' : null],
+                'required' => false,
+            ]);
 
         }
     }
@@ -31,6 +47,8 @@ class AddressForm extends AbstractType
         $resolver->setDefaults([
             'data_class' => AddressModel::class,
             'show_address_two' => false,
+            'show_labels' => true,
+            'show_placeholders' => true,
         ]);
     }
 }
